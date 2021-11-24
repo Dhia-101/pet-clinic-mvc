f8de4087857eaff751dd5a18d144036a98f8724e
DhiaTr <trabelsidhia19@gmail.com>
2021-11-24 13:08:20 +0100

    add AbstractMapService

---
+++ pet-clinic-data/src/main/java/com/dhia/springpetclinic/services/map/AsbtractMapService.java
@@ -0,0 +1,28 @@
+package com.dhia.springpetclinic.services.map;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public abstract class AsbtractMapService<T, ID> {
+    protected Map<ID, T> map = new HashMap<>();
+
+    Set<T> findAll() {
+      return new HashSet<>(map.values());
+    }
+
+    T save(ID id, T object) {
+        map.put(id, object);
+        return object;
+    }
+
+    void deleteById(ID id) {
+        map.remove(id);
+    }
+
+    void delete(T object) {
+        map.entrySet().removeIf(entry -> entry.getValue().equals(object));
+    }
+
+}
