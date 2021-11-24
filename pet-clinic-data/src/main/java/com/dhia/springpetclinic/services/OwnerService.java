e3345a2c11a6a157cbfce4b1caef5ddc8900784a
DhiaTr <trabelsidhia19@gmail.com>
2021-11-24 10:29:12 +0100

    refactor services crud code

--- pet-clinic-data/src/main/java/com/dhia/springpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/dhia/springpetclinic/services/OwnerService.java
@@ -2,13 +2,4 @@ package com.dhia.springpetclinic.services;
 
 import com.dhia.springpetclinic.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
-
-    Owner findById(Long id);
-
-    Owner save(Owner owner);
-
-    Set<Owner> findAll();
-}
+public interface OwnerService extends CrudService<Owner, Long> { }
