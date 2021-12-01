2511205eeaf31b91b1faa5ccc84e606aa9792609
DhiaTr <trabelsidhia19@gmail.com>
2021-12-01 20:40:51 +0100

    add cat and dog petTypes to bootstrap dataLoader

--- pet-clinic-app/src/main/java/com/dhia/springpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-app/src/main/java/com/dhia/springpetclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package com.dhia.springpetclinic.bootstrap;
 
 import com.dhia.springpetclinic.model.Owner;
+import com.dhia.springpetclinic.model.PetType;
 import com.dhia.springpetclinic.model.Vet;
 import com.dhia.springpetclinic.services.OwnerService;
+import com.dhia.springpetclinic.services.PetTypeService;
 import com.dhia.springpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -10,16 +12,26 @@ import org.springframework.stereotype.Component;
 @Component
 public class DataLoader implements CommandLineRunner {
 
-    private final OwnerService ownerSerice;
+    private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerSerice, VetService vetService) {
-        this.ownerSerice = ownerSerice;
+    public DataLoader(
+            OwnerService ownerService,
+            VetService vetService,
+            PetTypeService petTypeService
+    ) {
+        this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = petTypeService.save(new PetType("dog"));
+        PetType cat = petTypeService.save(new PetType("cat"));
+
         Owner owner1 = new Owner();
         owner1.setFirstName("firstNameOne");
         owner1.setLastName("lastNameOne");
@@ -28,15 +40,15 @@ public class DataLoader implements CommandLineRunner {
         owner2.setFirstName("firstNameTwo");
         owner2.setLastName("lastNameTwo");
 
-        ownerSerice.save(owner2);
+        ownerService.save(owner2);
 
         Owner owner3 = new Owner();
         owner3.setFirstName("firstNameThree");
         owner3.setLastName("lastNameThree");
 
-        ownerSerice.save(owner3);
+        ownerService.save(owner3);
 
-        System.out.println("Loaded Oweners...");
+        System.out.println("Loaded Owners...");
 
         Vet vet1 = new Vet();
         vet1.setFirstName("vetFirstNameOne");
