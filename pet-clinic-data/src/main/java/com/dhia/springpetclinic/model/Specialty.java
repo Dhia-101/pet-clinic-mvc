31a947fabb5b61c429b1fa37e3174875c122b842
DhiaTr <trabelsidhia19@gmail.com>
2021-12-03 12:31:21 +0100

    add specialties crud interface and mapService implementation

--- pet-clinic-data/src/main/java/com/dhia/springpetclinic/model/Specialty.java
+++ pet-clinic-data/src/main/java/com/dhia/springpetclinic/model/Specialty.java
@@ -1,6 +1,6 @@
 package com.dhia.springpetclinic.model;
 
-public class Specialty {
+public class Specialty extends BaseEntity {
 
     private String description;
 
---
+++ pet-clinic-data/src/main/java/com/dhia/springpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,5 @@
+package com.dhia.springpetclinic.services;
+
+import com.dhia.springpetclinic.model.Specialty;
+
+public interface SpecialtiesService extends CrudService<Specialty, Long> { }
