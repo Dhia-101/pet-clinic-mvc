57e565c1f1335a0347df859c9614e118d30fc569
DhiaTr <trabelsidhia19@gmail.com>
2021-12-07 21:11:46 +0100

    add PetSDJpaService implementation

---
+++ pet-clinic-data/src/main/java/com/dhia/springpetclinic/services/springDataJpa/PetSDJpaService.java
@@ -0,0 +1,48 @@
+package com.dhia.springpetclinic.services.springDataJpa;
+
+import com.dhia.springpetclinic.model.Pet;
+import com.dhia.springpetclinic.repositories.PetRepository;
+import com.dhia.springpetclinic.services.PetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springDataJPA")
+public class PetSDJpaService implements PetService {
+
+    private final PetRepository petRepository;
+
+    public PetSDJpaService(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        Set<Pet> pets = new HashSet<>();
+        petRepository.findAll().forEach(pets::add);
+        return pets;
+    }
+
+    @Override
+    public Pet findById(Long petId) {
+        return petRepository.findById(petId).orElse(null);
+    }
+
+    @Override
+    public Pet save(Pet pet) {
+        return petRepository.save(pet);
+    }
+
+    @Override
+    public void delete(Pet pet) {
+        petRepository.delete(pet);
+    }
+
+    @Override
+    public void deleteById(Long petId) {
+        petRepository.deleteById(petId);
+    }
+}
