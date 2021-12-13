baabf7e2bac943a353cb7f93d7335eadc2f194ee
DhiaTr <trabelsidhia19@gmail.com>
2021-12-13 13:08:32 +0100

    add owner templates; add ownerDetail basic page handler

--- pet-clinic-app/src/main/java/com/dhia/springpetclinic/controllers/OwnerController.java
+++ pet-clinic-app/src/main/java/com/dhia/springpetclinic/controllers/OwnerController.java
@@ -3,7 +3,10 @@ package com.dhia.springpetclinic.controllers;
 import com.dhia.springpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("owners")
@@ -21,4 +24,14 @@ public class OwnerController {
 
         return "notImplemented";
     }
+
+    @GetMapping("/{ownerId}")
+    public ModelAndView showOwner(@PathVariable("ownerId") Long ownerId) {
+        ModelAndView mav = new ModelAndView("owners/ownerDetails");
+        mav.addObject(this.ownerService.findById(ownerId));
+        return mav;
+    }
+
+
+
 }
