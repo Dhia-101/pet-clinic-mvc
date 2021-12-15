ecd062f5e96e435c1a5cf902aec89902076e5653
DhiaTr <trabelsidhia19@gmail.com>
2021-12-15 19:50:46 +0100

    add visit Controller: form init, processing function; fix few visit template issues

---
+++ pet-clinic-app/src/main/java/com/dhia/springpetclinic/controllers/VisitController.java
@@ -0,0 +1,58 @@
+package com.dhia.springpetclinic.controllers;
+
+import com.dhia.springpetclinic.model.Pet;
+import com.dhia.springpetclinic.model.Visit;
+import com.dhia.springpetclinic.services.PetService;
+import com.dhia.springpetclinic.services.VisitService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@Controller
+public class VisitController {
+
+    private final VisitService visitService;
+    private final PetService petService;
+
+    private static final String VIEWS_VISIT_CREATE_OR_UPDATE_FORM = "pets/createOrUpdateVisitForm";
+
+    public VisitController(VisitService visitService, PetService petService) {
+        this.visitService = visitService;
+        this.petService = petService;
+    }
+
+    @InitBinder
+    public void setAllowedFields(WebDataBinder dataBinder) {
+        dataBinder.setDisallowedFields("id");
+    }
+
+    @ModelAttribute("visit")
+    public Visit loadPetWithVisit(@PathVariable("petId") Long petId, Model model) {
+        Pet pet = this.petService.findById(petId);
+        model.addAttribute("pet", pet);
+        Visit visit = new Visit();
+        pet.getVisits().add(visit);
+        return visit;
+    }
+
+    @GetMapping("/owners/*/pets/{petId}/visits/new")
+    public String initNewVisitForm(@PathVariable("petId") Long petId, Model model) {
+        model.addAttribute("pet", new Pet());
+        return VIEWS_VISIT_CREATE_OR_UPDATE_FORM;
+    }
+
+    @PostMapping("/owners/{ownerId}/pets/{petId}/visits/new")
+    public String processNewVisitForm(@Valid Visit visit, BindingResult result) {
+        if (result.hasErrors()) {
+            return "pets/createOrUpdateVisitForm";
+        } else {
+            this.visitService.save(visit);
+            return "redirect:/owners/{ownersId}";
+        }
+    }
+}
+
