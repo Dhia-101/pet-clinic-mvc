aa47ee595beaf7a71240281c41629bc1125995db
DhiaTr <trabelsidhia19@gmail.com>
2021-12-21 10:56:23 +0100

    add get vets json api endpoint

--- pet-clinic-app/src/main/java/com/dhia/springpetclinic/controllers/VetController.java
+++ pet-clinic-app/src/main/java/com/dhia/springpetclinic/controllers/VetController.java
@@ -1,12 +1,15 @@
 package com.dhia.springpetclinic.controllers;
 
+import com.dhia.springpetclinic.model.Vet;
 import com.dhia.springpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
-@RequestMapping("/vets")
 public class VetController {
 
     private final VetService vetService;
@@ -15,10 +18,15 @@ public class VetController {
         this.vetService = vetService;
     }
 
-    @RequestMapping({"", "/", "index"})
+    @RequestMapping({"/vets", "/vets/", "/vets/index"})
     public String index(Model model) {
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";
+    }
+
+    @RequestMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetJson() {
+        return vetService.findAll();
     }
 
 }
